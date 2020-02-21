fce2aada933330397a372b83afab6728b0d0f9d7
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2020-02-21 15:47:38 +0000

    :zap: Fixed Patch to remove trees

--- VTOLVR-Multiplayer/Patches/Patch_VTTMapTrees.cs
+++ VTOLVR-Multiplayer/Patches/Patch_VTTMapTrees.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using HarmonyLib;
-[HarmonyPatch(typeof(VTTMapTrees), "Start")]
-public static class Patch_VTTMapTrees
+﻿using HarmonyLib;
+
+[HarmonyPatch(typeof(VTTMapTrees.TreeJob), "CreateTree")]
+public static class Patch_TreeMaster
 { 
     [HarmonyPrefix]
     public static bool Prefix()
     {
-        UnityEngine.Debug.LogWarning("Stopped Generating Trees");
         return false;
     }
 }
