f733311326cb777618805d040a05fef9d3b590e4
Nathan R <nproux@gmail.com>
2020-07-17 19:35:18 -0400

    - We can now set a mod version number in a file - The mod version is now checked on join, join is rejected on mismatch - Fixed source of nulls with radar

---
+++ VTOLVR-Multiplayer/ModVersionString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class ModVersionString
+{
+    public static string ReleaseBranch = "modVersionChecker";
+    public static string ModVersionNumber = "0.0.1";
+}
