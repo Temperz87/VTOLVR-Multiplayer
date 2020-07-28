4a9c9cf4d40b6ab83a0a4fd9d369da9fc8b6730f
omarehaly <omarehaly@gmail.com>
2020-07-28 18:15:57 +0100

    full networked action sync

---
+++ VTOLVR-Multiplayer/Networkers/ScenarioActionNetworker_Receiver.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class ScenarioActionNetworker_Receiver : MonoBehaviour
+{
+    public ulong networkUID;
+    private Message_ScenarioAction lastMessage;
+   
+   
+
+    private void Awake()
+    {
+        lastMessage = new Message_ScenarioAction(networkUID,0);
+        Networker.runScenarioAction += runScenarioAction;
+    }
+
+    public void runScenarioAction(Packet packet)
+    {
+        lastMessage = (Message_ScenarioAction)((PacketSingle)packet).message;
+
+        Debug.Log("recieved action from other");
+        // do not run scenarios on self
+        if (lastMessage.UID == PlayerManager.localUID)
+            return;
+        PlayerManager.runScenarioAction(lastMessage.scenarioActionHash);
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("Destroyed action syncer");
+        Debug.Log(gameObject.name);
+    }
+}
