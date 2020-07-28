81d3d42a0db3193b442ec98eb537d7361d406da2
DESKTOP-SM4PJFO\tempe <temperz871@gmail.com>
2020-07-27 20:45:18 -0400

    Added VTEventTarget patch

---
+++ VTOLVR-Multiplayer/Patches/Patch_VTEventTarget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+
+
+[HarmonyPatch(typeof(VTEventTarget), "Invoke")]
+class Patch1
+{
+    static void Postfix(VTEventTarget __instance)
+    {
+
+        Debug.Log("Host Ran  " + __instance.eventName + " of type" + __instance.methodName + " for target " + __instance.targetID);
+    }
+}
