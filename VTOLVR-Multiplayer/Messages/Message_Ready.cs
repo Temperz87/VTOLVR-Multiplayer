8264c576a202bcecc67ce4f6fb798b8b51975a7f
THE-GREAT-OVERLORD-LORD-OF-ALL-CHEESE <45265801+THE-GREAT-OVERLORD-LORD-OF-ALL-CHEESE@users.noreply.github.com>
2020-08-15 14:20:46 +0100

    showing team names, scaled text more to avoid falling off

--- VTOLVR-Multiplayer/Messages/Message_Ready.cs
+++ VTOLVR-Multiplayer/Messages/Message_Ready.cs
@@ -9,10 +9,12 @@ public class Message_Ready : Message
 {
     public ulong UID;
     public bool isHost;
+    public bool isLeft;
 
-    public Message_Ready(ulong uID, bool isHost) {
+    public Message_Ready(ulong uID, bool isHost, bool isLeft) {
         UID = uID;
         this.isHost = isHost;
+        this.isLeft = isLeft;
         type = MessageType.Ready;
     }
 }
