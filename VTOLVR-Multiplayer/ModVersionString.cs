6f55d603e25d5e791b834f6c01876720314118bc
DESKTOP-SM4PJFO\tempe <temperz871@gmail.com>
2020-09-06 21:44:44 -0400

    Update ModVersionString.cs

--- VTOLVR-Multiplayer/ModVersionString.cs
+++ VTOLVR-Multiplayer/ModVersionString.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 static class ModVersionString
 {
     public static string ReleaseBranch = "Release";
-    public static string ModVersionNumber = "1.8.0";
+    public static string ModVersionNumber = "1.8.1";
 }
