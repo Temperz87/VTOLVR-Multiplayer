687a4c1343febce434105c78f24ce7cdf2b230a0
omarehaly <omarehaly@gmail.com>
2020-09-18 00:51:55 +0100

    1.9.3

--- VTOLVR-Multiplayer/ModVersionString.cs
+++ VTOLVR-Multiplayer/ModVersionString.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 static class ModVersionString
 {
     public static string ReleaseBranch = "Release";
-    public static string ModVersionNumber = "1.9.2";
+    public static string ModVersionNumber = "1.9.3";
 }
