2fd6404bc36fdfa8d6113026219171f21549b51f
omarehaly <omarehaly@gmail.com>
2020-09-29 03:41:12 +0100

    freq

--- VTOLVR-Multiplayer/Multiplayer.cs
+++ VTOLVR-Multiplayer/Multiplayer.cs
@@ -188,7 +188,7 @@ public class Multiplayer : VTOLMOD
         thrust_changed += thrust_Settings;
         alpha_changed += alpha_Settings;
         settings.CreateCustomLabel("Thrust Multiplier");
-        settings.CreateFloatSetting("Default = 1.0", thrust_changed,1.0f, 1.0f, 4.0f, 0.2f);
+        settings.CreateFloatSetting("Default = 1.0", thrust_changed,1.0f, 1.0f, 5.0f, 0.2f);
         settings.CreateCustomLabel("High Alpha Mode");
         settings.CreateBoolSetting("Default = False", alpha_changed, alpha);
 
